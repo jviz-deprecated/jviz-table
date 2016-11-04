67e23e18a8f5c315f59ceff608796ddbffcd58c5
jmjuanes <josemijuanes@gmail.com>
2016-11-04 13:59:22 +0100

    js/page.js: fix

--- js/page.js
+++ js/page.js
@@ -26,47 +26,13 @@ jviz.modules.table.prototype.page = function(n)
   jviz.dom.val(this._page.counter.input.id, this._page.actual);
 
   //Get the entries text
-  var entries = this._page.entries.text.replace('{start}', this._page.rows.start + 1).replace('{end}', this._page.rows.end + 1);
+  var entries = this._page.entries.text.replace('{start}', this._data.start + 1).replace('{end}', this._data.end + 1);
 
   //Replace the total number of entries
-  entries = entries.replace('{total}', this._table.el.countRows());
+  entries = entries.replace('{total}', this._data.length);
 
   //Display the entries text
   jviz.dom.html(this._page.entries.id, entries);
-
-  //Continue
-  return this;
-};
-
-//Get o save the number of page rows
-jviz.modules.table.prototype.pageRows = function(n)
-{
-  //Check for undefined
-  if(typeof n === 'undefined'){ return this._page.rows.number; }
-
-  //Save the number
-  this._page.rows.number = parseInt(n);
-
-  //Check the number
-  if(this._page.rows.number <= 0){ this._page.rows.number = this._page.rows.number.default; }
-
-  //Calculate the number of pages
-  this._page.end = Math.floor(this._table.el._data.length / this._page.rows.number) + 1;
-
-  //Open the first page
-  this.page(1);
-
-  //Update the number of pages
-  jviz.dom.html(this._page.counter.label2.id, ' of ' + this._page.end);
-
-  //Update the min attribute
-  jviz.dom.attr(this._page.counter.input.id, 'min', this._page.start);
-
-  //Update the max attribute
-  jviz.dom.attr(this._page.counter.input.id, 'max', this._page.end);
-
-  //Update the step
-  jviz.dom.attr(this._page.counter.input.id, 'step', this._page.step);
 
   //Continue
   return this;
