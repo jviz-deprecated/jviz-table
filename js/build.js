c94698e0c697c9de83d29fa49dd81d0034639127
jmjuanes <josemijuanes@gmail.com>
2016-11-04 15:42:22 +0100

    js/build.js: update page counter label

--- js/build.js
+++ js/build.js
@@ -25,29 +25,41 @@ jviz.modules.table.prototype.build = function()
   //Build the page counter div
   jviz.dom.append(this._page.id, { id: this._page.counter.id, class: this._page.counter.class });
 
-  //Build the page counter label 1
-  jviz.dom.append(this._page.counter.id, { id: this._page.counter.label1.id, class: this._page.counter.label1.class });
+  //Build the page label
+  jviz.dom.append(this._page.counter.id, { _tag: 'span', id: this._page.counter.label.page.id });
 
   //Build the page counter input
   jviz.dom.append(this._page.counter.id, { _tag: 'input', type: 'number', id: this._page.counter.input.id, class: this._page.counter.input.class });
 
   //Build the page counter label 2
-  jviz.dom.append(this._page.counter.id, { id: this._page.counter.label2.id, class: this._page.counter.label2.class });
+  jviz.dom.append(this._page.counter.id, { tag: 'span', id: this._page.counter.label.total.id });
 
   //Build the previous button
   jviz.dom.append(this._page.id, { id: this._page.btn.prev.id, class: this._page.btn.prev.class });
 
-  //Add the label1 text
-  jviz.dom.html(this._page.counter.label1.id, this._page.counter.label1.text);
+  //Add the page label text
+  jviz.dom.html(this._page.counter.label1.id, this._page.counter.label.page.text);
 
-  //Add the label2 text
-  jviz.dom.html(this._page.counter.label2.id, this._page.counter.label2.text);
+  //Add the total label text
+  jviz.dom.html(this._page.counter.label2.id, this._page.counter.label.total.text);
 
   //Add the previous button text
   jviz.dom.html(this._page.btn.prev.id, this._page.btn.prev.text);
 
   //Add the next button text
   jviz.dom.html(this._page.btn.next.id, this._page.btn.next.text);
+
+  //Save this
+  var self = this;
+
+  //Page counter event
+  jviz.dom.on(this._page.counter.input.id, 'change', function(){ return self.pageChange(); });
+
+  //Add the next button event
+  jviz.dom.on(this._page.btn.next.id, 'click', function(){ return self.pageNext(); });
+
+  //Add the previous button event
+  jviz.dom.on(this._page.btn.prev.id, 'click', function(){ return self.pagePrev(); });
 
   //Continue
   return this;
