d776fac1f11fa318546acdd836bec1cdb42b0844
jmjuanes <josemijuanes@gmail.com>
2016-11-11 18:07:55 +0100

    js/info.js: update the page info

--- js/info.js
+++ js/info.js
@@ -1,3 +1,57 @@
+//Update the info
+jviz.modules.table.prototype.info = function()
+{
+  //Check if the checkbox element is visible
+  if(this._info.checkbox.visible === true)
+  {
+    //Get the number of checked rows
+    var count = this.countChecked();
+
+    //Get the text
+    var text = this._page.checked.text.replace('{check}', count);
+
+    //Add the text
+    jviz.dom.html(this._page.checked.id, text);
+
+    //Check the number
+    (count === 0) ? jviz.dom.hide(this._page.checked.id) : jviz.dom.show(this._page.checked.id);
+  }
+
+  //Check the total element
+  if(this._info.total.visible === true)
+  {
+    //Get the actual data length
+    var data_src = this._data.src.length;
+
+    //Get the filter data length
+    var data_filter = this._data.length;
+
+    //Update the total length
+    jviz.dom.append(this._info.total.id, this._info.total.text.replace('{total}', data_src));
+
+    //Check for show or hide
+    (data_src === data_filter) ? jviz.dom.hide(this._info.total.id) : jviz.dom.show(this._info.total.id);
+  }
+
+  //Get the entries text
+  var entries = this._info.showing.text;
+
+  //Add the start entrie
+  entries = entries.replace('{start}', this._draw.start + 1);
+
+  //Add the end entrie
+  entries = entries.replace('{end}', this._draw.end + 1);
+
+  //Replace the actual number of entries
+  entries = entries.replace('{actual}', this._data.length);
+
+  //Display the entries text
+  jviz.dom.html(this._info.showing.id, entries);
+
+  //Continue
+  return this;
+};
+
 //Build the info element
 jviz.modules.table.prototype.infoBuild = function()
 {
